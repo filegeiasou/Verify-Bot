a4f2b4ceb99b48e437bc104f6cd9b7f31560d4f3
filegeiasou <49124547+filegeiasou@users.noreply.github.com>
2019-05-13 22:45:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,6 @@ bot = commands.Bot(command_prefix="/", status=discord.Status.idle, activity=disc
 
 @bot.command()
 async def verify(ctx):
- await ctx.channel.send("Hellow)
+ await ctx.channel.send("Hello.")
 
 bot.run("NTc3NTQyMzM2MTE3MjExMTQ2.XNmmTg.hpekKwefCTL6FOr9LllnDySJ5gY")
