75fd23d9f4a78d2a98ee984d1881746823e94165
filegeiasou <49124547+filegeiasou@users.noreply.github.com>
2019-05-13 22:27:19 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,11 @@
+﻿import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = commands.Bot(command_prefix="/", status=discord.Status.idle, activity=discord.Game(name="Testing.."))
+
+@bot.command()
+async def verify(ctx):
+ await ctx.channel.send("Test message.")
+
+bot.run("NTc3NTQyMzM2MTE3MjExMTQ2.XNmmTg.hpekKwefCTL6FOr9LllnDySJ5gY")
