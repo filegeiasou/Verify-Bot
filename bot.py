228a84e327be25cb24edf872bef092886a74d451
filegeiasou <49124547+filegeiasou@users.noreply.github.com>
2019-05-14 22:19:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,5 +13,8 @@ async def pipe(ctx):
  await ctx.channel.send("Yes pipe.")
 if (ctx==pipe):
  print("https://discordapp.com/api/oauth2/authorize?client_id=577542336117211146&permissions=0&scope=bot"
+else:
+ print("Pipe")
+       
  
 bot.run("NTc3NTQyMzM2MTE3MjExMTQ2.XNmmTg.hpekKwefCTL6FOr9LllnDySJ5gY")
