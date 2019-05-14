0795b1e6ee5cdf5b3582e94a72c789d3e4cd8754
filegeiasou <49124547+filegeiasou@users.noreply.github.com>
2019-05-14 21:47:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,12 @@ bot = commands.Bot(command_prefix="/", status=discord.Status.idle, activity=disc
 @bot.command()
 async def verify(ctx):
  await ctx.channel.send("Hello.")
+ 
+bot = commands.Bot(command_prefix="/", status=discord.Status.idle,
+                   
+                   
+@bot.command()
+async def pipe(ctx):
+ await ctx.channel.send("Yes pipe.")
 
 bot.run("NTc3NTQyMzM2MTE3MjExMTQ2.XNmmTg.hpekKwefCTL6FOr9LllnDySJ5gY")
