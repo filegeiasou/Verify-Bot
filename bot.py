ae1a1a175e0535016d981aeea474773f806f02ed
filegeiasou <49124547+filegeiasou@users.noreply.github.com>
2019-05-16 20:39:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,17 +35,5 @@ async def wlu(ctx):
 
  await ctx.channel.send(embed=testembed)
 
-@bot.command()
-async def now(ctx):
- testembed = discord.Embed(
-   title = "Verify bot",
-   description = " Verify bot  and site here : http://verifynow.cf/.",
-   colour = discord.Colour.red()
- )
- 
- testembed.set_author(name="Verifynow")
- testembed.set_footer(text="Wlu team")
-
- await ctx.channel.send(embed=testembed)
  
 bot.run("NTc3NTQyMzM2MTE3MjExMTQ2.XNmmTg.hpekKwefCTL6FOr9LllnDySJ5gY")
