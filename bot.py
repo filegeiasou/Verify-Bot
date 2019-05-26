3837998d7a29d2ca64d3b28b5ac9e3f77503c5b9
filegeiasou <49124547+filegeiasou@users.noreply.github.com>
2019-05-26 17:27:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,27 +26,22 @@ async def on_member_remove(member):
    
 @bot.command()
 async def userinfo(ctx,member: discord.Member):
+ embed = discord.Embed(color=member.color, timestamp=ctx.message.created_at)
+  
+ embed.set_author(name=f"User Info - {member}")
+ embed.set_thumbnail(url=member.avatar_url)
+ embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
    
-   roles = [role for role in member.roles]
+ embed.add_field(name="ID:", value=member.id)
+ embed.add_field(name="Guild name:", value=member.display_name)
+ embed.add_field(name="Created at:", value=memebr.created_at.strftime("%a , %#d %B %Y, %I:%M %p UTC"))
+ embed.add_field(name="Joined at:", value=member.joined_at.strftime("%a , %#d %B %Y, %I:%M %p UTC"))
+ 
+ embed.add_field(name="Top role:", value=member.top_role.mention)
    
-   embed = discord.Embed(color=member.color, timestamp=ctx.message.created_at)
+ embed.add_field(name"Bot?", value=member.bot)
    
-   embed.set_author(name=f"User Info - {member}")
-   embed.set_thumbnsil(url=member.avatar_url)
-   embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-   
-   embed.add_field(name="ID:", value=member.id)
-   embed.add_field(name="Guild name:", value=member.display_name)
-   
-   embed.add_field(name="Created at:", value=memebr.created_at.strftime("%a , %#d %B %Y, %I:%M %p UTC"))
-   embed.add_field(name="Joined at:", value=member.joined_at.strftime("%a , %#d %B %Y, %I:%M %p UTC"))
-   
-   embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
-   embed.add_field(name="Tope role:", value=member.top_role.mention)
-   
-   embed.add_field(name"Bot?", value=member.bot)
-   
-   await ctx.send(embed=embed)
+ await ctx.send(embed=embed)
 
 
 
