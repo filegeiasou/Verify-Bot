076ff8a52925131abbf29ed40a8399ad9326fd4e
filegeiasou <49124547+filegeiasou@users.noreply.github.com>
2019-05-26 17:10:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,8 @@ async def on_member_remove(member):
    
    
 @Bot.command()
-async def userinfo(ctx,member: discord.Member):
+async def userinfo(ctx,member: discord.Member = None):
+ member=ctx.author if not member else member
  embed = discord.Embed(color=member.color, timestamp=ctx.message.created_at)
   
  embed.set_author(name=f"User Info - {member}")
